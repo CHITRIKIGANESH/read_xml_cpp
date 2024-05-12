2a48cbc1d33ef5294469f6312370cb92d0bef5f9
Chitriki Ganesh <104210702+CHITRIKIGANESH@users.noreply.github.com>
2024-05-12 13:40:21 +0530

    changed regex pattern

--- Sample/read_xml_fstream.cpp
+++ Sample/read_xml_fstream.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 void RemoveTags(string s)
 {
-	const regex pattern("\\<.*?\\>");
+	//const regex pattern("\\<.*?\\>");
+
+	const regex pattern("\\<");
+	const regex patt("/.*");
+	const regex pat("\\>");
+	
 
 	s = regex_replace(s, pattern, "");
+	s = regex_replace(s, patt, "");
+	s = regex_replace(s, pat, ": ");
 
 	cout << s << "\n";
 
@@ -29,45 +36,4 @@ int main () {
       cout << "File opening is fail."<<endl;   
     }  
   return 0;  
-}  
-
-
-
-
-/*
-// C++ program for the above approach
-#include <iostream>
-#include <regex>
-using namespace std;
-
-// Function to remove the HTML tags
-// from the given string
-void RemoveHTMLTags(string s)
-{
-	const regex pattern("\\<.*?\\>");
-
-	// Use regex_replace function in regex
-	// to erase every tags enclosed in <>
-	s = regex_replace(s, pattern, "");
-
-	// Print string after removing tags
-	cout << s;
-
-	return;
 }
-
-// Driver Code
-int main()
-{
-
-	// Given String
-	string str = "<div><b>Geeks for Geeks</b></div>";
-
-	// Function call to print the
-	// HTML string after removing tags
-	RemoveHTMLTags(str);
-
-	return 0;
-}
-*/
-// This code is contributed by yuvraj_chandra
